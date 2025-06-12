5ef5cbe22b5d68813710dfa175dc97c589aa641f
Torres Pablo <eneporene25@gmail.com>
2025-06-12 01:02:17 -0300

    Incorporo botón para añadir favorito de cada producto y estado por defecto de favorito a false ,cuando se agrega o lee productos, Torres Pablo Gabriel

--- src/assets/contexts/ProductosContext.jsx
+++ src/assets/contexts/ProductosContext.jsx
@@ -12,15 +12,18 @@ export function ProductosProvider({ children }) {
         datosProductos.map(producto => ({
             ...producto,
             estado: producto.estado ? producto.estado.toLowerCase() : 'activo',
+            favorito: producto.favorito ?? false,
         }))
     );
+
+    const [favoritos, setFavoritos] = useState([]);
 
     //Funciones para modificar la lista de productos
     const agregarProducto = useCallback((nuevoProducto) => {
         setProductos((prevProductos) => {
             const nuevoId = String(prevProductos.length > 0 ?
                 Math.max(...prevProductos.map(a => Number(a.id))) + 1 : 1);
-            return [...prevProductos, { ...nuevoProducto, id: nuevoId, estado: 'activo' }];
+            return [...prevProductos, { ...nuevoProducto, id: nuevoId, estado: 'activo', favorito: false }];
         });
     }, []);
     // Aqui colocaremos las demas funciones como editar, eliminar(ocultarEstado), mostrar,etc
@@ -38,13 +41,24 @@ export function ProductosProvider({ children }) {
         );
     }, []);
 
+    const favoritoProducto = useCallback((idProducto) => {
+        setProductos(prevProductos =>
+            prevProductos.map(producto =>
+                producto.id === idProducto
+                    ? { ...producto, favorito: !producto.favorito }
+                    : producto
+            )
+        );
+    }, []);
+
     // Memorizamos el valor del contexto para optimizar
     const contextValue = useMemo(() => ({
         productos,
         setProductos,
         agregarProducto,
         eliminarProducto,
-    }), [productos, agregarProducto, eliminarProducto,]);
+        favoritoProducto,
+    }), [productos, agregarProducto, eliminarProducto, favoritoProducto]);
 
     return (
         <ProductosContext.Provider value={contextValue}>
---
+++ src/assets/pages/Favoritos.jsx
@@ -0,0 +1,2 @@
+// Página de Favoritos:
+// Mostrar únicamente los productos que el usuario ha marcado como favoritos.
